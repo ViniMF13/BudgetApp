c9a95cfd87143c44c7808aa79510edc1dbd533c8
Vinicius Faria <viniciusfaria.vmf@gmail.com>
2023-03-19 23:09:46 -0300

    add historic

--- script.js
+++ script.js
@@ -34,7 +34,6 @@ addIncome.addEventListener("click", () => {
     if(incomeName == '' || incomeValue == '' || incomeCategory == '' || incomeDate == ''){
       return
     }
-
     const newItem = {
         name: incomeName,
         value: incomeValue,
@@ -49,13 +48,14 @@ addIncome.addEventListener("click", () => {
          newInput.innerHTML = "<h3>"+ newInput.name +"</h3>" + "<p>"+ newInput.category +"</p>" + "<h3>" + `${newInput.value} R$` + "</h3>" + "<p>"+ newInput.date +"</p>"
          newInput.classList.add(newItem.type)
          inputsList.appendChild(newInput)
+         
   
     localStorage.setItem("inputData", JSON.stringify(inputData))
 
-    document.getElementById("incomeName").value = "";
-    document.getElementById("incomeValue").value = "";
-    document.getElementById("investmentDate").value = "";
-    document.getElementById("investmentCategory").value = "";
+    document.getElementById("incomeName").value = ""
+    document.getElementById("incomeValue").value = ""
+    document.getElementById("investmentDate").value = ""
+    document.getElementById("investmentCategory").value = ""
 })
 
 addExpense.addEventListener("click", () => {
@@ -110,13 +110,13 @@ addInvestment.addEventListener("click", () => {
     type: "investment"
   } 
 
-  inputData.inputs.push(newItem)
 
   const newInput = document.createElement("li");
      newInput.innerHTML = "<h3>"+ newInput.name +"</h3>" + "<p>"+ newInput.category +"</p>" + "<h3>" + `${newInput.value} R$` + "</h3>" + "<p>"+ newInput.date +"</p>"
      newInput.classList.add(newItem.type)
      inputsList.appendChild(newInput)
 
+  inputData.inputs.push(newItem)
   localStorage.setItem("inputData", JSON.stringify(inputData))
 
   document.getElementById("investmentName").value = "";
@@ -140,7 +140,6 @@ const showIncomes = document.getElementById('totalIncomes')
 showIncomes.innerHTML = '<h3>' + totalIncomes + '</h3>'  + "<h3> R$ </h3>"
 
 for(let i = 0; i < inputData.inputs.length; i++){
-
   if (inputData.inputs[i].type == "expense"){
     let expense = parseInt(inputData.inputs[i].value)
     totalExpenses = totalExpenses + expense
@@ -160,11 +159,51 @@ const showInvestments = document.getElementById('totalInvestments')
 showInvestments.innerHTML = '<h3>' + totalInvestments + '</h3>' + "<h3> R$ </h3>"
 
 
-
-
-
-
 const showResults = document.getElementById('showResults')
+
+let historicData = []
+
+let storedHistoric = localStorage.getItem("historicData")
+if (storedHistoric) {
+  historicData = JSON.parse(storedHistoric)
+  showResults.classList.remove('none')
+}
+
+for(let i = 0; i < historicData.length; i++){
+  const newPeriod = document.createElement('h3')
+  newPeriod.innerHTML = `De: ${historicData[i].period[0]} até ${historicData[i].period[1]}`
+
+  const newChart = document.createElement('canvas')
+  var ctx = newChart.getContext('2d');
+  var data = {
+    labels: ['income', 'expense', 'investments'],
+    datasets: [{
+    data: [historicData[i].total[0], historicData[i].total[1], historicData[i].total[2]],
+    backgroundColor: ['#00a67d', '#f22c3d', '#7922eb',]
+    }]
+  }
+  var myPieChart = new Chart(ctx, {
+    type: 'pie',
+    data: data,
+    options: {
+      responsive: true,
+      maintainAspectRatio: true
+    }
+  })
+
+  const InputsHistoric = document.createElement('ul')
+  for(let j = 0; j < historicData[i].inputs.length; j++){
+    const newInput = document.createElement("li");
+    newInput.classList.add(historicData[i].inputs[j].type)
+    newInput.innerHTML = "<h5>" + historicData[i].inputs[j].name + "</h5>" + "<h5>" + `${historicData[i].inputs[j].value} R$` + "</h5>";
+    InputsHistoric.appendChild(newInput)
+  }
+
+  showResults.appendChild(newPeriod)
+  showResults.appendChild(newChart)
+  showResults.appendChild(InputsHistoric)
+}
+
 
 const resultButton = document.getElementById('resultsButton')
 resultButton.addEventListener('click', ()=>  {
@@ -173,13 +212,22 @@ resultButton.addEventListener('click', ()=>  {
 
   const periodStart = document.getElementById('periodStart').value
   const periodEnd = document.getElementById('periodEnd').value
-  const newPeriod = document.createElement('h3')
-  newPeriod.innerHTML = `De: ${periodStart} até ${periodEnd}`
 
+  let resultData = {
+    period: [periodStart, periodEnd],
+    inputs: inputData.inputs,
+    total: [totalIncomes, totalExpenses, totalInvestments]
+  }
+  
+  historicData.push(resultData)
+  localStorage.setItem("historicData", JSON.stringify(historicData))
+
+
+  const newPeriod = document.createElement('h3')
+  newPeriod.innerHTML = `De: ${resultData.period[0]} até ${resultData.period[1]}`
 
   const newChart = document.createElement('canvas')
   var ctx = newChart.getContext('2d');
-
   var data = {
     labels: ['income', 'expense', 'investments'],
     datasets: [{
@@ -194,45 +242,56 @@ resultButton.addEventListener('click', ()=>  {
       responsive: true,
       maintainAspectRatio: true
     }
-   })
+  })
 
   const InputsHistoric = document.createElement('ul')
-
   for(let i = 0; i < inputData.inputs.length; i++){
-  const newInput = document.createElement("li");
-  newInput.classList.add(inputData.inputs[i].type)
-  newInput.innerHTML = "<h5>" + inputData.inputs[i].name + "</h5>" + "<h5>" + `${inputData.inputs[i].value} R$` + "</h5>";
-  InputsHistoric.appendChild(newInput)
-}
+    const newInput = document.createElement("li");
+    newInput.classList.add(inputData.inputs[i].type)
+    newInput.innerHTML = "<h5>" + inputData.inputs[i].name + "</h5>" + "<h5>" + `${inputData.inputs[i].value} R$` + "</h5>";
+    InputsHistoric.appendChild(newInput)
+  }
 
   showResults.appendChild(newPeriod)
   showResults.appendChild(newChart)
   showResults.appendChild(InputsHistoric)
   
-  historicData = inputData
-  localStorage.setItem("historicData", JSON.stringify(historicData))
+  })
 
-  inputData = {
-    inputs: {}
-  }
 
-})
+  //const newPeriod = document.createElement('h3')
+  //newPeriod.innerHTML = `De: ${periodStart} até ${periodEnd}`
+
+
+
+ /*  
+
+  x 
+ 
+
+
+  
+
+  
+  
+  
+*/
 
 
 
 /*
-let historicData = {
+let resultData = {
   totals: [],
   inputs: []
 }
 
-let storedHistoric = localStorage.getItem("historicData")
+let storedHistoric = localStorage.getItem("resultData")
 if (storedHistoric) {
-  storedHistoric = JSON.parse(historicData)
+  storedHistoric = JSON.parse(resultData)
   showResults.classList.remove('none')
 }
 
-for(let i = 0; i < historicData.size.length; i++){
+for(let i = 0; i < resultData.size.length; i++){
  console.log()
 }
 */
